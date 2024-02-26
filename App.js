c59ebb350ad29fa77823eb6a98459a318f1f59da
rokas <rokas@rokas.com>
2024-02-26 12:52:53 +0200

    Add pipe, base, and bird images to the game screen

--- App.js
+++ App.js
@@ -12,12 +12,51 @@ const App = () => {
   const { width, height } = useWindowDimensions();
   const bg = useImage(require('./assets/sprites/background-day.png'));
   const bird = useImage(require('./assets/sprites/yellowbird-upflap.png'));
+  const pipeBottom = useImage(require('./assets/sprites/pipe-green.png'));
+  const pipeTop = useImage(require('./assets/sprites/pipe-green-top.png'));
+  const base = useImage(require('./assets/sprites/base.png'));
+
+  const pipeOffset = 0
+
+
   const r = width * 0.33;
 
   return (
     <Canvas style={{ width, height }}>
+      {/* BG */}
       <Image image={bg} width={width} height={height} fit={'cover'} />
-      <Image image={bird} x={width / 2} y={height / 2} width={r} height={r} />
+      {/* Pipes */}
+      <Image
+        image={pipeTop}
+        y={pipeOffset - 320}
+        x={width / 2}
+        width={103}
+        height={640}
+      />
+      <Image
+        image={pipeBottom}
+        y={height - 320 + pipeOffset}
+        x={width / 2}
+        width={103}
+        height={640}
+      />
+      {/* Base */}
+      <Image
+        image={base}
+        width={width}
+        height={150}
+        y={height - 75}
+        x={0}
+        fit={'cover'}
+      />
+      {/* Bird */}
+      <Image
+        image={bird}
+        x={width / 4}
+        y={height / 2}
+        width={64}
+        height={48}
+      />
     </Canvas>
   );
 };
