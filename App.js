321cf9aa6d48496fd580d398006a2d03cd76f7fb
rokas <rokas@rokas.com>
2024-02-26 12:39:12 +0200

    Add bird image to canvas component

--- App.js
+++ App.js
@@ -11,11 +11,13 @@ import {
 const App = () => {
   const { width, height } = useWindowDimensions();
   const bg = useImage(require('./assets/sprites/background-day.png'));
+  const bird = useImage(require('./assets/sprites/yellowbird-upflap.png'));
   const r = width * 0.33;
 
   return (
     <Canvas style={{ width, height }}>
       <Image image={bg} width={width} height={height} fit={'cover'} />
+      <Image image={bird} x={width / 2} y={height / 2} width={r} height={r} />
     </Canvas>
   );
 };
