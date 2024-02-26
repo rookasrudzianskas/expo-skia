d80c042cfa1dce9689300e642d02d25197be9eb8
rokas <rokas@rokas.com>
2024-02-26 12:32:02 +0200

    Update App.js with dynamic canvas dimensions

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Canvas, Circle, Group } from "@shopify/react-native-skia";
+import {useWindowDimensions} from "react-native";
 
 const App = () => {
-  const width = 256;
-  const height = 256;
+  const { width, height } = useWindowDimensions();
   const r = width * 0.33;
+
   return (
-    <Canvas style={{ width, height }}>
+    <Canvas style={{ width, height, backgroundColor: 'red' }}>
       <Group blendMode="multiply">
         <Circle cx={r} cy={r} r={r} color="cyan" />
         <Circle cx={width - r} cy={r} r={r} color="magenta" />
