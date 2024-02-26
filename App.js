c42d1a7ccad7494241c7bc370fb3541afd697940
rokas <rokas@rokas.com>
2024-02-26 12:36:41 +0200

    Add background image to Canvas component

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
 import React from "react";
-import { Canvas, Circle, Group } from "@shopify/react-native-skia";
-import {useWindowDimensions} from "react-native";
-
+import { Platform, useWindowDimensions } from 'react-native';
+import {
+  Canvas,
+  useImage,
+  Image,
+  Group,
+  Text,
+  matchFont,
+} from '@shopify/react-native-skia';
 const App = () => {
   const { width, height } = useWindowDimensions();
+  const bg = useImage(require('./assets/sprites/background-day.png'));
   const r = width * 0.33;
 
   return (
-    <Canvas style={{ width, height, backgroundColor: 'red' }}>
-      <Group blendMode="multiply">
-        <Circle cx={r} cy={r} r={r} color="cyan" />
-        <Circle cx={width - r} cy={r} r={r} color="magenta" />
-        <Circle cx={width / 2} cy={width - r} r={r} color="yellow" />
-      </Group>
+    <Canvas style={{ width, height }}>
+      <Image image={bg} width={width} height={height} fit={'cover'} />
     </Canvas>
   );
 };
